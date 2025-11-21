ca667f03e858bf0f88b64f70f6d9d3e6991b8799
min <minfeel0214@gmail.com>
2025-11-21 17:24:52 +0900

    min

--- oracle.db/1120_min.sql
+++ oracle.db/1120_min.sql
@@ -2,3 +2,5 @@
 create table Mymallipgo(num number(3) primary key, category varchar2(20), sangpum varchar2(30), su number(5), price number(10), ipgo date);
 
 select * from mymallipgo;
+
+create table MystudentInfo(stu_no number(3) primary key, stu_name varchar2(15), stu_addr varchar2(30), stu_grade number(5), stu_blood varchar2(10), stu_birth varchar2(20), writeday date);
